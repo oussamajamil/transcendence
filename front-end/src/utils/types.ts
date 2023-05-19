a6cf4f19803ddf90209445ffdda5c438e4ff5543
drabi-he <drabi-he@debian-BULLSEYE-live-builder-AMD64>
2023-05-19 22:09:28 +0100

    history

---
+++ front-end/src/utils/types.ts
@@ -0,0 +1,18 @@
+export type matchHistoryType = {
+  id: string;
+  winner: userType;
+  loser: userType;
+  winnerScore: number;
+  loserScore: number;
+  createdAt: Date;
+};
+
+export type userType = {
+  id: string;
+  login: string;
+  avatar: string;
+
+  winHistory: matchHistoryType[];
+  loseHistory: matchHistoryType[];
+  rank: "bronze" | "silver" | "gold";
+};
