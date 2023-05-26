439a5a78f57568c6111776308a050d8b843033c0
Mohamed Mayaz <momayaz@e1r10p11.1337.ma>
2023-05-26 10:28:30 +0100

    message page

--- front-end/src/pages/messages/conversationList.tsx
+++ front-end/src/pages/messages/conversationList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./conversationList.scss";
 import { ReactComponent as ConvImage } from "../../assets/hero5.svg";
 import { ReactComponent as Online } from "../../assets/online.svg";
-import {Avatar} from 'rsuite';
+
 interface Props {
   id: number;
   name: string;
@@ -10,19 +10,6 @@ interface Props {
 }
 
 function ConversationListPage(props: Props) {
-  const [Color, setColor] = React.useState("#baccfd");
-  const [BgColor, setbgColor] = React.useState("#f4f6ff");
-
-  const handleButtonClick = () => {
-    setColor("#1657FF");
-    setbgColor("#baccfd");
-  };
-
-  const [isActive, setIsActive] = React.useState(false);
-  
-  const handleClick = () => {
-    setIsActive(true);
-  };
 
   let text = props.message;
   if (text.length > 20) {
@@ -35,16 +22,13 @@ function ConversationListPage(props: Props) {
 
   return (
     <div
-      className=" chat-container-cmp flex items-center  gap-[3rem] cursor-pointer h-[100px] min-w-[400px] hover:bg-[#BACCFD] "
+      className=" chat-container-cmp flex items-center  gap-[3rem] cursor-pointer h-[100px] min-w-[400px] hover:bg-[#BACCFD]  "
     >
       <div className="flex  items-center  h-[100%] ">
-        <Avatar
-          src="https://as2.ftcdn.net/v2/jpg/01/24/30/67/1000_F_124306753_5lrF4qCOZLL2wFsyFfaJf6FprTTtcvnX.jpg"
-          className=" ms-[4rem] my-[1rem] chat-img rounded-[50%] w-[70px] h-[70px]  "
-        />
+        <ConvImage className=" ms-[4rem] my-[1rem] chat-img rounded-[50%] w-[70px] h-[70px]  " />
         <Online className=" online-icon -translate-y-8 -translate-x-5 " />
       </div>
-      <div className="text flex justify-evenly flex-col gap-[1rem] ">
+      <div className="text flex justify-evenly flex-col gap-[1rem] " >
         <h1 className=" name ">{name}</h1>
         <p className="msg  ">{text}</p>
       </div>
--- front-end/src/pages/messages/index.tsx
+++ front-end/src/pages/messages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./style.scss";
 import { ReactComponent as LoopeIcon } from "../../assets/loopicon.svg";
+import { ReactComponent as ConvImage } from "../../assets/hero5.svg";
 import { useState } from "react";
 import ConversationListPage from "./conversationList";
 import GroupsList from "./groupsList";
-import { Avatar} from 'rsuite';
 
 function MessagesPage() {
   const [Color1, setColor1] = React.useState("#000000");
@@ -84,88 +84,88 @@ function MessagesPage() {
       name: "fskxzckdsc",
       message: "Hello",
     },
-    // {
-    //   id: 12,
-    //   name: "fskxzckdsc",
-    //   message: "Hello",
-    // },
-    // {
-    //   id: 13,
-    //   name: "fskxzckdsc",
-    //   message: "Hello",
-    // },
-    // {
-    //   id: 14,
-    //   name: "fskxzckdsc",
-    //   message: "Hello",
-    // },
-    // {
-    //   id: 15,
-    //   name: "fskxzckdsc",
-    //   message: "Hello",
-    // },
+    {
+      id: 12,
+      name: "fskxzckdsc",
+      message: "Hello",
+    },
+    {
+      id: 13,
+      name: "fskxzckdsc",
+      message: "Hello",
+    },
+    {
+      id: 14,
+      name: "fskxzckdsc",
+      message: "Hello",
+    },
+    {
+      id: 15,
+      name: "fskxzckdsc",
+      message: "Hello",
+    },
   ]);
 
   const [groups, setGroups] = useState([
     {
-      id: 1,
+      id: 111,
       name: "dfckzcldnscxzsfgksfgk;sdjkfgjksd",
       message:
         "Hellodfghdfghndfghkldfgjhdfklghjkdfghdfklghdfklgkldfgjhkldfgjhkldfgkhdfgh",
     },
     {
-      id: 2,
+      id: 211,
       name: "fscxjz",
       message: "Hello",
     },
     {
-      id: 3,
+      id: 311,
       name: "sfnxzc",
       message: "Hello",
     },
     {
-      id: 4,
+      id: 411,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 5,
+      id: 511,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 5,
+      id: 511,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 6,
+      id: 611,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 7,
+      id: 711,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 8,
+      id: 811,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 9,
+      id: 911,
       name: "fskxzckdsc",
       message: "Hello",
     },
     {
-      id: 10,
+      id: 1011,
       name: "fskxzckdsc",
       message: "Hello",
     },
 
     {
-      id: 11,
+      id: 1111,
       name: "fskxzckdsc",
       message: "Hello",
     },
@@ -193,7 +193,7 @@ function MessagesPage() {
 
   const [msg, setMsg] = useState([
     {
-      id: 1,
+      id: 122,
       name: "dfckzcldnscxzsfgksfgk;sdjkfgjksd",
       messages: [
         "Hellodfghdfghndfghkldfgjhdfklghjkdfghdfklghdfklgkldfgjhkldfgjhkldfgkhdfgh",
@@ -207,31 +207,31 @@ function MessagesPage() {
       dist: "in",
     },
     {
-      id: 2,
+      id: 222,
       name: "fscxjz",
       messages: ["Hello"],
       dist: "out",
     },
     {
-      id: 3,
+      id: 322,
       name: "sfnxzc",
       messages: ["Hello"],
       dist: "in",
     },
     {
-      id: 4,
+      id: 422,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "out",
     },
     {
-      id: 5,
+      id: 522,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "in",
     },
     {
-      id: 5,
+      id: 522,
       name: "fskxzckdsc",
       messages: [
         "Hellodfghdfghndfghkldfgjhdfklghjkdfghdfklghdfklgkldfgjhkldfgjhkldfgkhdfgh",
@@ -245,38 +245,38 @@ function MessagesPage() {
       dist: "out",
     },
     {
-      id: 6,
+      id: 622,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "in",
     },
     {
-      id: 7,
+      id: 722,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "out",
     },
     {
-      id: 8,
+      id: 822,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "in",
     },
     {
-      id: 9,
+      id: 922,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "out",
     },
     {
-      id: 10,
+      id: 1022,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "in",
     },
 
     {
-      id: 11,
+      id: 1122,
       name: "fskxzckdsc",
       messages: [
         "Hellodfghdfghndfghkldfgjhdfklghjkdfghdfklghdfklgkldfgjhkldfgjhkldfgkhdfgh",
@@ -290,19 +290,19 @@ function MessagesPage() {
       dist: "out",
     },
     {
-      id: 12,
+      id: 1222,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "in",
     },
     {
-      id: 13,
+      id: 1322,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "out",
     },
     {
-      id: 14,
+      id: 1422,
       name: "fskxzckdsc",
       messages: [
         "Hellodfghdfghndfghkldfgjhdfklghjkdfghdfklghdfklgkldfgjhkldfgjhkldfgkhdfgh",
@@ -316,7 +316,7 @@ function MessagesPage() {
       dist: "in",
     },
     {
-      id: 15,
+      id: 1522,
       name: "fskxzckdsc",
       messages: ["Hello"],
       dist: "out",
@@ -324,10 +324,11 @@ function MessagesPage() {
   ]);
 
   return (
-    <div className="game-container bg-[#FFFFFF] rounded-[50px] flex items-center justify-center flex-col  h-[90%] ">
-      <h1 className="h1">Chat / Messages</h1>
+    <div className="min-w-[50vh] w-full h-[92vh]  ">
+    <div className="game-container bg-[#FFFFFF] rounded-[50px] flex items-center justify-center flex-col  h-[90%] w-full  gap-4 ">
+      <h1 className="h1 w-full h-[3rem] ">Chat / Messages</h1>
       <div className=" chat-container  flex items-center justify-center gap-[3rem] w-[95%] h-[90%] py-4  ">
-        <div className="conversations w-[30%] rounded-[30px] flex  flex-col  h-full">
+        <div className="conversations w-[30%] rounded-[30px]  flex  flex-col  h-[100%] overflow-hidden ">
           <div className="conversation-header w-[100%] h-[8rem] flex justify-evenly ">
             <button
               className=" chat-btn "
@@ -352,7 +353,7 @@ function MessagesPage() {
               className="search-input w-[100%] h-[100%] outline-none"
             />
           </div>
-          <div className="flex flex-col overflow-y-auto h-[80%] conv">
+          <div className="flex flex-col overflow-y-auto h-[80%]  conv">
             {tab === false
               ? messages.map((message) => (
                   <ConversationListPage
@@ -374,12 +375,9 @@ function MessagesPage() {
         </div>
         <div className="chat w-[65%] rounded-[30px] h-[100%] flex items-center justify-center  flex-col  ">
           <h1 className=" no-cnv ">choose a chat to start the conversation</h1>
-          <div className=" chat-header w-[100%] h-[10%] flex justify-between  ">
-            <div className=" h-[100%] flex items-center justify-center left-container gap-3 ">
-              <Avatar
-                src="https://as2.ftcdn.net/v2/jpg/01/24/30/67/1000_F_124306753_5lrF4qCOZLL2wFsyFfaJf6FprTTtcvnX.jpg"
-                className=" ms-[4rem] my-[1rem] chat-img rounded-[50%] w-[70px] h-[70px]  "
-              />
+          <div className=" chat-header w-[100%] h-[10%] flex justify-between ">
+            <div className=" con h-[100%] flex items-center justify-center left-container gap-3 ">
+              <ConvImage className=" ms-[4rem] my-[1rem] chat-img rounded-[50%] w-[70px] h-[70px] avat " />
               <div className=" left h-[100%] flex items-center justify-center flex-col ">
                 <h2 className=" user-name ">user name</h2>
                 <p className=" cnx-status ">online/offline</p>
@@ -387,74 +385,65 @@ function MessagesPage() {
             </div>
             <div className=" right-container h-[100%]  flex items-center justify-center  ">
               <button className=" t-point  ">...</button>
-              <div className=" w-[60px] h-[60px] bg-[#BACCFD] rounded-[100%] -translate-x-10 "></div>
+              <div className=" w-[60px] h-[60px] bg-[#BACCFD] rounded-[100%] -translate-x-10 cir "></div>
             </div>
           </div>
-          <div className=" chat-body w-[70%] h-[70%] overflow-scroll gap-1 ">
+          <div className=" chat-body w-[70%] h-[70%] overflow-scroll gap-1   ">
             {msg.map((message) =>
               message.dist === "in" ? (
-                <div className="chat-message" key={message.id}>
+                <div className="chat-message" >
                   <div className="flex items-end">
                     <div className="flex flex-col space-y-2 text- max-w-xs mx-2 order-2 items-start">
-                      {message.messages.map((msge, index) => (
+                      {message.messages.map((msge) => (
                         <div className=" ">
                           <span
-                            className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gradient-to-br from-blue-600 to-red-500 text-white"
-                            key={index}
+                            className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-white  text-[#515261]"
                           >
                             {msge}
                           </span>
                         </div>
                       ))}
                     </div>
-                    <Avatar
-                      src="https://as2.ftcdn.net/v2/jpg/01/24/30/67/1000_F_124306753_5lrF4qCOZLL2wFsyFfaJf6FprTTtcvnX.jpg"
-                      className=" w-12 h-12 rounded-full order-1  "
-                    />
+                    <ConvImage className=" w-12 h-12 rounded-full order-1  " />
                   </div>
                 </div>
               ) : (
-                <div className="chat-message" key={message.id}>
+                <div className="chat-message">
                   <div className="flex items-end justify-end">
                     <div className="flex flex-col space-y-2 text-lg max-w-xs mx-2 order-1 items-end">
-                      {message.messages.map((msg, index) => (
+                      {message.messages.map((msg) => (
                         <div>
                           <span
-                            className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-gradient-to-tr from-red-600 via-transparent to-blue-300"
-                            key={index}
+                            className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-[#CEDDFE] "
                           >
                             {msg}
                           </span>
                         </div>
                       ))}
                     </div>
-                    <Avatar
-                      src="https://as2.ftcdn.net/v2/jpg/01/24/30/67/1000_F_124306753_5lrF4qCOZLL2wFsyFfaJf6FprTTtcvnX.jpg"
-                      className=" w-12 h-12 rounded-full order-2  "
-                    />
+                    <ConvImage className=" w-12 h-12 rounded-full order-2  " />
                   </div>
                 </div>
               )
             )}
           </div>
-          <div className=" chat-footer w-[80%] h-[15%] ">
+          <div className=" chat-footer w-[80%] h-[10%] ">
             <div className="relative flex w-full h-full">
               <input
-                type="text"
+                type="textarea"
                 placeholder="Write your message!"
-                className="w-full h-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+                className="w-[70%] h-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-[#C4C4C4] pl-12 bg-white rounded-[10px] py-3 overflow-hidden"
               />
-              <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+              <div className="absolute right-0 items-center inset-y-0 flex">
                 <button
                   type="button"
                   className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
                 >
-                  <span className="font-bold">Send</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
-                    className="h-6 w-6 ml-2 transform rotate-90"
+                    className="h-10 w-10 ml-2 transform rotate-90"
                   >
                     <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
                   </svg>
@@ -464,6 +453,7 @@ function MessagesPage() {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
