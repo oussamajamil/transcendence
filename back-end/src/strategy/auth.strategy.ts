af4d00d20508795c5486e9baeb5217fe13d2e024
Oussama Jamil <ojamil@e1r4p9.1337.ma>
2023-06-12 19:01:46 +0100

    oauth intra

--- back-end/src/strategy/auth.strategy.ts
+++ back-end/src/strategy/auth.strategy.ts
@@ -3,16 +3,17 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-42';
 
 @Injectable()
-export class AuthStrategy extends PassportStrategy(Strategy, '42') {
+export class AuthStrategy extends PassportStrategy(Strategy, 'inta42') {
   constructor() {
     super({
-      clientID: 'YOUR_CLIENT_ID',
-      clientSecret: 'YOUR_CLIENT_SECRET',
-      callbackURL: 'YOUR_CALLBACK_URL',
+      clientID: 'u-s4t2ud-5c48f31801a9f617ed1429f64263480d6b8e7a81dd75c844015597a54cd02952',
+      clientSecret: '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',
+      callbackURL: 'http://localhost:3000/auth/profile',
     });
   }
 
   async validate(accessToken: string, refreshToken: string, profile: any): Promise<any> {
+    console.log("i am here");
     // Here, you can perform custom validation or save the user to the database
     console.log({accessToken, refreshToken, profile});
     
